6f5f203284e765228f2904e9c100bbd0b6db807e
ilnthulara <nethu0905@gmail.com>
2023-11-05 11:25:59 +1000

    Adding files

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBMI] = useState(null);
+
+  const calculateBMI = () => {
+    if (weight && height) {
+      const heightInMeters = height / 100;
+      const bmiValue = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+      setBMI(bmiValue);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>BMI Calculator</h1>
+      <div>
+        <label>Weight (kg):</label>
+        <input
+          type="number"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Height (cm):</label>
+        <input
+          type="number"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+      </div>
+      <button onClick={calculateBMI}>Calculate BMI</button>
+      {bmi && <h2>Your BMI is: {bmi}</h2>}
+    </div>
+  );
+};
+
+export default App;
